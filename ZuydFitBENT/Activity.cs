77c9ecf11883787ca1d3a30f2035d05bb5889b1b
MarcDerks <32189920+MarcDerks@users.noreply.github.com>
2024-04-09 11:24:31 +0200

    Update and rename Training.cs to Activity.cs

---
+++ ZuydFitBENT/Activity.cs
@@ -0,0 +1,19 @@
+class Activity
+{
+    public int id { private set; get; }
+    public string name { private set; get; }
+    public DateTime date { private set; get; }
+    public string location { private set; get; }
+    public Program program { private set; get; }
+    public List<Trainer> trainers { private set; get;}
+
+    public Activity(int Id, string Name, DateTime Date, string Location, Program Program)
+    {
+        id = Id;
+        name = Name;
+        date = Date;
+        location = Location;
+        program = Program;
+        trainers = new List<Trainer>();
+    }
+}
