cb04ffc77ae1833eddc19f3a60f20bf151aa0c91
MarcDerks <32189920+MarcDerks@users.noreply.github.com>
2024-04-09 11:23:34 +0200

    Update Student.cs

--- ZuydFitBENT/Student.cs
+++ ZuydFitBENT/Student.cs
@@ -1,33 +1,19 @@
-public class Student
+class Participant
 {
-    public string name { get; private set; }
-    public int studentNumber { get; private set; }
-    public List<Goal> goals { get; private set; }
-    public string emailAdress { get; private set; }
-  
-    public Student(string Name, int StudentNumber, string EmailAdress)
+    public int id { private get; set; }
+    public string name { private get; set; }
+    public string gender { private get; set; }
+    public string email { private get; set; }
+    public string phone { private get; set; }
+    public List<RosterParticipant> rosterParticipants { private get; set; }
+
+    public Participant(int Id, string Name, string Gender, string Email, string Phone)
     {
+        id = Id;
         name = Name;
-        studentNumber = StudentNumber;
-        goals = new List<Goal>();
-        emailAdress = EmailAdress;
+        gender = Gender;
+        email = Email;
+        phone = Phone;
+        rosterParticipants = new List<RosterParticipant>();
     }
-    public void AddGoal(Goal goal)
-    {
-        goals.Add(goal);
-    }
-    public void RemoveGoal(Goal goal) 
-    {  
-        players.Remove(goal); 
-    }
-    public void DisplayCurrentGoals(Student student)
-    {
-        Console.WriteLine("The student's current goals are:");
-        foreach(Goal goal in goals)
-        {
-          if(goals.achieved == false)
-          {
-            Console.WriteLine(Goal.description);
-          }
-        }
-    }
+}
